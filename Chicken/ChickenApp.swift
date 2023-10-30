5c7c82568e2784c9bd74164e5bce83c1a9be6b6f
angela <angela@userde-MacBook-Pro.local>
2023-10-30 21:35:35 +0800

    Initial Commit

---
+++ Chicken/ChickenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenApp.swift
+//  Chicken
+//
+//  Created by angela on 2023/10/30.
+//
+
+import SwiftUI
+
+@main
+struct ChickenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
